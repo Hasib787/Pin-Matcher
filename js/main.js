65e69aad6db1ddc12f4add2c17f37e410bfa5b92
Hasib787 <mr.hasib45@gmail.com>
2021-02-01 17:49:26 +0600

    added a calculate function

--- js/main.js
+++ js/main.js
@@ -9,8 +9,23 @@ function getPin() {
     }
 }
 
+
 //display generate pic
 function generatePin() {
     const pinInput = document.getElementById('pin');
     pinInput.value = getPin();
 }
+
+
+//handle calculator button event
+const buttonContainer = document.getElementById('digits-container');
+buttonContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        //handle backspace
+        //handle clear
+    } else {
+        const typedInput = document.getElementById('typed-pin');
+        typedInput.value = typedInput.value + digit;
+    }
+})
